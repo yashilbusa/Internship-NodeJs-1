722d91a4c43c0e05d3f4d7aa541eeaf9b67dab47
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 15:07:01 +0530

    complete cjs module

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
+const PersonalDetail = require('./second');
+
 console.log("Hello World");
+
+console.log(PersonalDetail);
---
+++ second.js
@@ -0,0 +1,10 @@
+module.exports.Detail = {
+    Name :"Yashil Busa",
+    Age : 21,
+    City : "Jamnagar",
+    FavShow : "TMKOC",
+    Single : true
+}
+
+// OR 
+// module.exports = Detail
