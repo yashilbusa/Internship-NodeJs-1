4c9562c01f82109f5c9ef789b3f44e0d5911c0b2
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 16:19:01 +0530

    c

--- fsModule.js
+++ fsModule.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 
-fs.readFile('file.txt' , 'utf8' , (err,data)=>{
-    console.log(data);
-    console.log(err);
-})
+
+//Example of Non-Blocking IO Model
+// It will read file but it will also not blocking remaing code so it will print first 
+// the console then print file data
+
+// fs.readFile('file.txt' , 'utf8' , (err,data)=>{
+//     console.log(data);
+//     console.log(err);
+// })
+// console.log("Finishing Reading File"); 
+
+
+
+
+// Without Non Blocking IO Model we use readfileSync
+const data = fs.readFileSync('file.txt');
+console.log(data.toString());
+console.log("Finishing Reading File"); 
+
+
