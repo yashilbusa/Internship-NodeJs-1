056f5f87ffd64aa1e9b8faaea419bdde4b2e04e5
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 17:15:57 +0530

    c

--- fsModulePromise.js
+++ fsModulePromise.js
@@ -2,11 +2,12 @@ const fs = require('fs/promises');
 
 async function fsPromise() {
   try {
-    const data = "Some data!";
-    await fs.writeFile('/Users/joe/test.txt', data);
+    const data = "Some data of WriteFile Using Async/Await";
+    await fs.writeFile('WriteFilePromise.txt', data);
   } catch (err) {
     console.log(err);
   }
 }
+console.log("File Writing Successfully");
 
 fsPromise();
