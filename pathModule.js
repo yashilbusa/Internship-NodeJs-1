51a2841905bb5ea72f1ada6e57322c4954079595
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 15:56:19 +0530

    commit

--- pathModule.js
+++ pathModule.js
@@ -1,3 +1,11 @@
 const path = require('path');
 
-console.log(path.basename('C:\\temp\\myfile.html'));
+console.log(__dirname)
+console.log(__filename);;
+
+console.log(path.basename('C:\\temp\\myfile.html'));          // 'C:\temp\myfile.html' 
+console.log(path.extname('index.html'));        //.html
+console.log(path.extname(__filename));          //.js
+console.log(path.isAbsolute(__filename));       //true
+console.log(path.parse(__filename));
+
