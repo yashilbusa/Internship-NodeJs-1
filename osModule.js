0acbd04ee7109a84238ffdf2a3fb8ac27565d1fc
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 15:37:02 +0530

    c

--- osModule.js
+++ osModule.js
@@ -6,5 +6,3 @@ console.log(os.machine());      // x86_64
 console.log(os.type());         // Linux
 console.log(os.totalmem());     // Ram in Bytes : 16721780736 is Equivalent to 16.72 Gigabytes
 console.log(os.freemem());      // It Shows Free Ram Memory in bytes
-
-
