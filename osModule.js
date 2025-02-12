63ae98bf19ce4703e274217414efee7484e1f6b9
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 15:32:44 +0530

    osmodule learn

--- osModule.js
+++ osModule.js
@@ -1,1 +1,10 @@
 const os = require('os');
+
+console.log(os.homedir());      // /home/yashil_busa
+console.log(os.hostname());     // innovatemr
+console.log(os.machine());      // x86_64
+console.log(os.type());         // Linux
+console.log(os.totalmem());     // Ram in Bytes : 16721780736 is Equivalent to 16.72 Gigabytes
+console.log(os.freemem());      // It Shows Free Ram Memory in bytes
+
+
