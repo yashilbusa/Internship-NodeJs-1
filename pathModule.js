8156d6dd64d4d9384a4ecf00c5ec1dbc25ead673
yashilbusa <yashilpatel1914@gmail.com>
2025-02-12 16:04:34 +0530

    fs module

--- pathModule.js
+++ pathModule.js
@@ -7,5 +7,13 @@ console.log(path.basename('C:\\temp\\myfile.html'));          // 'C:\temp\myfile
 console.log(path.extname('index.html'));        //.html
 console.log(path.extname(__filename));          //.js
 console.log(path.isAbsolute(__filename));       //true
+
 console.log(path.parse(__filename));
+/*  {
+  root: '/',
+  dir: '/home/yashil_busa/Yashil Intenrship/Node Projects/Node-1',
+  base: 'pathModule.js',
+  ext: '.js',
+  name: 'pathModule'
+}   */
 
