4d7a956a066d98fb13eed82e3f7b5043356bae7f
yashilbusa <yashilpatel1914@gmail.com>
2025-02-13 09:22:13 +0530

    commit

--- fs_AppendFile.js
+++ fs_AppendFile.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
-console.log("\nFile Contents of file before append :",
+console.log("\nBefore append :",
     fs.readFileSync("example.txt", "utf8"));
 
-fs.appendFile("example.txt", "Yashil Busa", (err) => {
+fs.appendFile("example.txt", " Yashil Busa", (err) => {
     if (err) {
         console.log(err);
     }
     else {
-        console.log("\nFile Contents of file after append :",
+        console.log("\nAfter append :",
         fs.readFileSync("example.txt", "utf8"));
     }
 }); 
