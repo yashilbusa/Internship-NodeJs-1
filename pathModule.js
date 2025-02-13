1bed0d33ac5eec3f10f8b63d7db7f6f77d46689a
yashilbusa <yashilpatel1914@gmail.com>
2025-02-13 10:00:34 +0530

    c

--- pathModule.js
+++ pathModule.js
@@ -6,7 +6,7 @@ console.log(__filename);
 console.log(path.basename('C:\\temp\\myfile.html'));      // 'C:\temp\myfile.html' 
 console.log(path.extname('index.html'));        //.html
 console.log(path.extname(__filename));          //.js
-console.log(path.isAbsolute(__filename));       //true
+// console.log(path.isAbsolute(__filename));       //true
 
 console.log(path.parse(__filename));
 /*  {
