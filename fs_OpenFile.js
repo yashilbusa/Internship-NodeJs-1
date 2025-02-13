fb6fa1d41e75f7bdbc7acd641d59c5ef3005673f
yashilbusa <yashilpatel1914@gmail.com>
2025-02-13 10:18:20 +0530

    c

--- fs_OpenFile.js
+++ fs_OpenFile.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
-fs.open("readFile.txt", "r", function (err, data) {
+fs.open("readFile.txt", "r", (err, fd)=> {
     if (err) {
         return console.error(err);
     }
-    console.log(data);
+    console.log(fd);
     console.log("File open successfully");
 });
