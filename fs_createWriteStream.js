bc9db7ab0ab96a25f53da02242086d522dba4c70
yashilbusa <yashilpatel1914@gmail.com>
2025-02-13 11:47:39 +0530

    commit

--- fs_createWriteStream.js
+++ fs_createWriteStream.js
@@ -1,5 +1,10 @@
 const fs = require('fs'); 
 
 const writer = fs.createWriteStream('sample.txt') 
-
 writer.write('Hello Yashil From InnovateMR');
+
+
+
+// const writer1 = fs.createWriteStream('largeFile.txt');
+// const reader = fs.createReadStream('largeFile.txt').pipe(writer1);
+
